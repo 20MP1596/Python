32a53cf33db88b714107fcc90682bd13339c75fd
cucia <77478799+20MP1596@users.noreply.github.com>
2021-02-16 00:02:39 +0530

    Lab Cycle 1.3 added

--- #Lab_Cycle 1.2#3.py
+++
@@ -1,7 +0,0 @@
-names=["sasi","ramanan","jose"]
-count=0
-for name in names:
-    for letters in names:
-        if letters=="a" or letters=="A":
-            count=count+1
-print("Number of A's in the list :",count)
